bd6e016ef78f2d01a75556869c2678d08f30af90
a375D <liulanqix@163.com>
2020-04-30 04:18:57 +0800

    /api/conference/day:tags:search

--- backend/src/main/java/com/mobilecourse/backend/controllers/ConferenceController.java
+++ backend/src/main/java/com/mobilecourse/backend/controllers/ConferenceController.java
@@ -4,81 +4,71 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.mobilecourse.backend.dao.ConferenceDao;
 import com.mobilecourse.backend.dao.UserDao;
+import com.mobilecourse.backend.model.Conference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
 public class ConferenceController {
 
     //TODO: 在数据库中把note改成TEXT/MEDIUMTEXT/LONGTEXT类型
-    //TODO: 在conference中加入FULLTEXT以支持全文查找
-    //TODO: 学习链接：https://blog.csdn.net/qq_38377190/article/details/80873621；
-    // https://www.cnblogs.com/devcjq/articles/6340111.html
-    // https://www.jb51.net/article/28679.htm
+    //TODO: 在查询所有的conference时均要要求visible=1，否则即为未公开的
     @Autowired
     private ConferenceDao conferenceMapper;
 
-//    public String getConferenceInfos(String type, String keyword) {
-//        int index;
-//        switch (type) {
-//            case "day":
-//                index = 0;
-//                break;
-//            case "tags":
-//                index = 1;
-//                break;
-//            case "search":
-//                index = 2;
-//                break;
-//            default:
-//                index = -1;
-//        }
-//        if (index == -1) return "{ accepted: 0, msg: getConferenceInfos meet unknown query }";
-//        String base = "com.mobilecourse.backend.model.";
-//        String[] names = { "note", "message", "conference" };
-//        String queryparam = gtype.substring(0, gtype.length()-1);
-//        if (queryparam.equals("establishe")) queryparam = "establish";
-//        String[] entities = { base+"Note", base+"Message", base+"Conference" };
-//        String[] methods = { "selectAllNotes", "selectAllMessages", "selectAllUserConferences" };
-//        String[][] attrs = {
-//                { "note_id", "title", "text", "create_time", "update_time" },
-//                { "message_id", "details", "time" },
-//                { "conference_id", "organization", "introduction", "date",
-//                        "chairs", "place", "start_time", "end_time", "tags", "visible" }};
-//        Class<?> Info, Dao;
-//        ArrayList<Object> infos;
-//        try {
-//            Info = Class.forName(entities[index]);
-//            Dao = UserDao.class;
-//            if (index == 2) {
-//                Method method = Dao.getMethod(methods[index], int.class, String.class);
-//                infos = (ArrayList<Object>) method.invoke(userMapper, id, queryparam);
-//            } else {
-//                Method method = Dao.getMethod(methods[index], int.class);
-//                infos = (ArrayList<Object>) method.invoke(userMapper, id);
-//            }
-//            JSONArray allinfos = new JSONArray();
-//            for (Object info: infos) {
-//                JSONObject jsoninfo = new JSONObject();
-//                for (String attr: attrs[index]) {
-//                    String getAttr = "get" + Character.toUpperCase(attr.charAt(0)) + attr.substring(1);
-//                    jsoninfo.put(attr, Info.getMethod(getAttr).invoke(info));
-//                }
-//                allinfos.add(jsoninfo);
-//            }
-//
-//            JSONObject resp = new JSONObject();
-//            resp.put(names[index] + "s", allinfos);
-//            resp.put(names[index] + "_num", infos.size());
-//            return resp.toJSONString();
-//        } catch (Exception e) {
-//            return "{ accepted: 0, msg: " + e.getMessage() + " }";
-//        }
-//    }
+    @RequestMapping(value = "/api/conference/{type}")
+    public String getConferenceInfos(@PathVariable(value = "type") String type,
+                                     HttpServletRequest request) {
+        String param;
+        if (type.equals("search")) param = request.getParameter("keyword");
+        else param = request.getParameter(type);
+        if (param == null) return "{ accepted: 0, msg: param not found. }";
+        int index;
+        switch (type) {
+            case "day":
+                index = 0;
+                break;
+            case "tags":
+                index = 1;
+                break;
+            case "search":
+                index = 2;
+                break;
+            default:
+                index = -1;
+        }
+        if (index == -1) return "{ accepted: 0, msg: getConferenceInfos meet unknown query }";
+        String[] methods = { "selectByDate", "selectByTags", "selectByKeywords" };
+        String[] attrs = { "conference_id", "organization", "introduction", "date",
+                "chairs", "place", "start_time", "end_time", "tags", "visible" };
+        try {
+            List<Conference> infos = (List<Conference>) conferenceMapper.getClass()
+                    .getMethod(methods[index], String.class).invoke(conferenceMapper, param);
+            JSONArray allinfos = new JSONArray();
+            for (Conference info: infos) {
+                JSONObject jsoninfo = new JSONObject();
+                for (String attr: attrs) {
+                    String getAttr = "get" + Character.toUpperCase(attr.charAt(0)) + attr.substring(1);
+                    jsoninfo.put(attr, Conference.class.getMethod(getAttr).invoke(info));
+                }
+                allinfos.add(jsoninfo);
+            }
+
+            JSONObject resp = new JSONObject();
+            resp.put("conferences", allinfos);
+            resp.put("conference_num", infos.size());
+            return resp.toJSONString();
+        } catch (Exception e) {
+            return "{ accepted: 0, msg: " + e.getMessage() + " }";
+        }
+    }
 
 }
