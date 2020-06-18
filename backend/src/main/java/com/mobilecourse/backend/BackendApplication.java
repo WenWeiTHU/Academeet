02cd442b5e28aa28c357e43f6e934d3f34f78a9a
a375D <liulanqix@163.com>
2020-06-18 23:44:56 +0800

    merge

--- backend/src/main/java/com/mobilecourse/backend/BackendApplication.java
+++ backend/src/main/java/com/mobilecourse/backend/BackendApplication.java
@@ -1,17 +1,33 @@
 package com.mobilecourse.backend;
 
+import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.websocket.server.WsSci;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.mobilecourse.backend.dao")
 public class BackendApplication extends SpringBootServletInitializer {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
 
     // 程序入口
     public static void main(String[] args) {
